fe4210ad79693d11ea0f3b47da29574e8a7ad243
tomerke <tomersadi@gmail.com>
2022-01-28 12:43:36 +0200

    end of section6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
   model: any = {};
  
 
-  constructor(public accoutService: AccountService) { }
+  constructor(public accoutService: AccountService, private router: Router, private toastr: ToastrService ) { }
 
   ngOnInit(): void {
    
@@ -20,15 +22,16 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accoutService.login(this.model).subscribe(response => {
-      console.log(response);     
+     this.router.navigateByUrl('/members')   
     }, error => {
       console.error(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout() {
     this.accoutService.logout();
-   
+    this.router.navigateByUrl('/')   
   }
 
  
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
   model: any = {};
  
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -25,6 +26,7 @@ this.accountService.register(this.model).subscribe(response =>
     this.cancel();
   }, error =>{
     console.error(error);
+    this.toastr.error(error.error);
   });
 } 
 
