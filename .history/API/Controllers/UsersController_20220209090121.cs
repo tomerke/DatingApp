53b9b06b3527de4e05bcf1e8d853ed567fc54151
tomerke <tomersadi@gmail.com>
2022-02-09 12:48:34 +0200

    section137

---
+++ .history/API/Controllers/UsersController_20220209090121.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using System.Collections.Generic;
+using API.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
+using System.Security.Claims;
+using API.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Controllers
+{
+
+    [Authorize]
+    public class UsersController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _photoService = photoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            // var users = await _userRepository.GetUsersAsymc();
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
+            // var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            // return Ok(usersToReturn);
+        }
+
+
+
+        [HttpGet("{username}", Name="GetUser")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            //   var user = await _userRepository.GetUserByUserNameAsync(username)  ;
+            var user = await _userRepository.GetMemberAsync(username);
+            return user;
+            // return _mapper.Map<MemberDto>(user);
+        }
+
+        [Authorize]
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var user = await _userRepository.GetUserByUserNameAsync(User.GetUsername());
+
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsymc()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
+
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _userRepository.GetUserByUserNameAsync(User.GetUsername());
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null)
+            {
+                return BadRequest(result.Error.Message);
+            }
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0)
+            {
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsymc())
+            {
+                 return  CreatedAtRoute("GetUser",new {username = user.UserName},_mapper.Map<PhotoDto>(photo)) ;//_mapper.Map<PhotoDto>(photo);
+            }
+
+
+            return BadRequest("problem adding photo");
+        }
+
+    [HttpPut("set-main-photo/{photoId}")]
+    {
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserBy
+        }
+    }
+
+
+    }
+}
---
+++ .history/client/src/app/_services/members.service_20220209121629.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {  map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+import { Member } from '../_models/member';
+
+// const httpOptios = {
+//   headers: new HttpHeaders({
+//     Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
+//     // Authorization: 'Bearer '
+//   })
+// }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl =  environment.apiUrl;
+  members:Member[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(map(members =>{
+      this.members = members;
+      return members;
+    }));
+  }
+
+  getMember(username: string) {
+    const member = this.members.find(member => member.username === username);
+    if (member !== undefined) return of(member);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username );
+  }
+
+  updateMember(member: Member){
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
+  }
+
+
+  setMainPhoto(photoId: number){
+    return this.http.put(this.baseUrl + '')
+  }
+
+
+}
