d28e21230c7fd5dbf9a2a65217d439035ef0f558
tomerke <tomersadi@gmail.com>
2022-02-21 09:35:40 +0200

    end of section13

---
+++ .history/client/src/app/_modules/shared.module_20220221082246.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+import {TabsModule} from 'ngx-bootstrap/tabs'
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import {ButtonModule} from 'ngx-bootstrap'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule,
+    FileUploadModule ,
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot()
+  ],
+  exports: [BsDropdownModule, ToastrModule, TabsModule, PaginationModule,
+            NgxGalleryModule, FileUploadModule, BsDatepickerModule]
+})
+export class SharedModule { }
