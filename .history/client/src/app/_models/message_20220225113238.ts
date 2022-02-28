dafba3eeb00d3f7592c6ecc0fbac03a12f2db2ad
tomerke <tomersadi@gmail.com>
2022-02-28 12:35:34 +0200

    temp

--- .history/client/src/app/_models/message_20220225113238.ts
+++
@@ -1,9 +0,0 @@
-senderUserName: string;
-senderPhotoUrl: string;
-recipientId: number;
-recipientUserName: string;
-recipientPhotoUrl: string;
-content: string;
-dateRead?: any;
-messageSent: Date;
-}
